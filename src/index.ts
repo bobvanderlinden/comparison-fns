c5d5f84776208525ea63d76c06badcfa39bd2d17
Bob van der Linden <bobvanderlinden@gmail.com>
2024-11-22 19:13:59 +0100

    docs: add examples for all functions

--- src/index.ts
+++ src/index.ts
@@ -29,6 +29,10 @@ function isNumber(value: unknown): value is number {
 
 /**
  * Consideres all values equal, thus this does not change the order.
+ * @example
+ * ```js
+ * [1, 2, 3].sort(compareEqual) // [1, 2, 3]
+ * ```
  */
 function compareEqual<T>(a: T, b: T): CompareResult {
   return 0;
@@ -36,6 +40,13 @@ function compareEqual<T>(a: T, b: T): CompareResult {
 
 /**
  * Order values that pass a condition before anything else.
+ * @example
+ * The following will order NaN values first, followed by the remaining values sorted in ascending order.
+ * NaN values are considered equal to eachother, so `compareEqual` is used for them.
+ *
+ * ```js
+ * [3, 1, NaN, 2].sort(conditionFirstComparer(isNaN, compareEqual, compareNumber)) // [NaN, 1, 2, 3]
+ * ```
  */
 export function conditionFirstComparer<T>(
   condition: (value: T) => boolean,
@@ -57,8 +68,23 @@ export function conditionFirstComparer<T>(
 
 /**
  * Order values that pass a type guard before anything else.
+ * This operates equal to `conditionFirstComparer`, but with a type guard instead of a condition.
  * Optionally trueComparer can be provided that is applied to any value that passes the type guard.
  * Optionally falseComparer can be provided that is applied to any value that does not pass the type guard.
+ * @example
+ * The following will order non-null values first, followed by null values.
+ * When one of the compared values is a string, it is ordered higher than null.
+ * When both compared values are strings, they are ordered using `compareString`.
+ * When both compared values are not strings (thus null), they are compared using `compareEqual`.
+ *
+ * ```js
+ * const comparer: Comparer<string | null> = conditionTypeFirstComparer(
+ *   (value: string | null): value is string => value !== null,
+ *   compareString, // This is of type Comparer<string>
+ *   compareEqual
+ * );
+ * ["a", null, "b"].sort(comparer); // ["a", "b", null]
+ * ```
  */
 export function conditionTypeFirstComparer<TIn, TOut extends TIn = TIn>(
   condition: (value: TIn) => value is TOut,
@@ -80,6 +106,12 @@ export function conditionTypeFirstComparer<TIn, TOut extends TIn = TIn>(
 
 /**
  * Order a specific constant value before anything else.
+ * @example
+ * This will order a single value first. The rest will remain in their original order.
+ *
+ * ```js
+ * [3, 5, 2, 1, 4].sort(constFirstComparer(3)); // [3, 1, 2, 5, 4]
+ * ```
  */
 export function constFirstComparer<TConstant, TResult extends TConstant>(
   value: TConstant
@@ -90,6 +122,13 @@ export function constFirstComparer<TConstant, TResult extends TConstant>(
 /**
  * Order null and undefined values first and use comparer for other cases.
  * null and undefined are considered equal to eachother.
+ * @example
+ * The following will order null and undefined values first, the remaining items are ordered using `compareNumber`.
+ * Note that the null and undefined values remain in their original order.
+ *
+ * ```js
+ * [3, undefined, null, 2, undefined, 1].sort(nullableFirstComparer(compareNumber)); // [undefined, null, undefined, 1, 2, 3]
+ * ```
  */
 export function nullableFirstComparer<T>(
   comparer: Comparer<T>
@@ -104,6 +143,13 @@ export function nullableFirstComparer<T>(
 /**
  * Order null and undefined values last and use comparer for other cases.
  * null and undefined are considered equal to eachother.
+ * @example
+ * The following will order null and undefined values last, the remaining items are ordered using `compareNumber`.
+ * Note that the null and undefined values remain in their original order.
+ *
+ * ```js
+ * [3, undefined, null, 2, undefined, 1].sort(nullableLastComparer(compareNumber)); // [3, 2, 1, undefined, null, undefined]
+ * ```
  */
 export function nullableLastComparer<T>(
   comparer: Comparer<T>
@@ -118,26 +164,56 @@ export function nullableLastComparer<T>(
 /**
  * Order null and undefined values before anything else.
  * null and undefined are considered equal to eachother.
+ * Other values remain in their original order.
+ * @example
+ * The following will order null and undefined values first.
+ *
+ * ```js
+ * [3, undefined, null, 2, undefined, 1].sort(compareNullableFirst); // [undefined, null, undefined, 3, 2, 1]
+ * ```
  */
 export const compareNullableFirst = nullableFirstComparer(compareEqual);
 
 /**
  * Order NaN values before anything else.
+ * Other values remain in their original order.
+ * @example
+ *
+ * ```js
+ * [3, 1, NaN, 2].sort(compareNanFirst); // [NaN, 3, 1, 2]
+ * ```
  */
 export const compareNanFirst = conditionFirstComparer(isNaN);
 
 /**
  * Order infinity values before anything else.
+ * Other values remain in their original order.
+ * @example
+ *
+ * ```js
+ * [3, 1, Infinity, 2].sort(comparePositiveInfinityFirst); // [Infinity, 3, 1, 2]
+ * ```
  */
 export const comparePositiveInfinityFirst = constFirstComparer(Infinity);
 
 /**
  * Order negative infinity values before anything else.
+ * Other values remain in their original order.
+ * @example
+ *
+ * ```js
+ * [3, 1, -Infinity, 2].sort(compareNegativeInfinityFirst); // [-Infinity, 3, 1, 2]
+ * ```
  */
 export const compareNegativeInfinityFirst = constFirstComparer(-Infinity);
 
 /**
  * Compare finite numbers. NaN, infinity, and negative infinity are considered equal.
+ * @example
+ *
+ * ```js
+ * [3, 1, NaN, 2].sort(compareFiniteNumber); // [1, 2, 3, NaN]
+ * ```
  */
 export function compareFiniteNumber(a: number, b: number) {
   return isFinite(a) && isFinite(b) ? sign(a - b) : 0;
@@ -149,6 +225,12 @@ export function compareFiniteNumber(a: number, b: number) {
  * - Negative infinity
  * - Finite numbers
  * - Positive infinity
+ *
+ * @example
+ *
+ * ```js
+ * [3, Infinity, 1, NaN, 2, -Infinity].sort(compareNumber); // [NaN, -Infinity, 1, 2, 3, Infinity]
+ * ```
  */
 export const compareNumber = chainComparers(
   compareNanFirst,
@@ -159,6 +241,11 @@ export const compareNumber = chainComparers(
 
 /**
  * Order strings using the current locale.
+ * @example
+ *
+ * ```js
+ * ["a", "b", "c"].sort(compareString); // ["a", "b", "c"]
+ * ```
  */
 export function compareString(a: string, b: string): CompareResult {
   return sign(a.localeCompare(b));
@@ -166,6 +253,12 @@ export function compareString(a: string, b: string): CompareResult {
 
 /**
  * Order strings using a specific locale.
+ * @example
+ *
+ * ```js
+ * ['Z', 'a', 'z', 'ä'].sort(localeComparer("de")); // ["a", "ä", "z", "Z"]
+ * ['Z', 'a', 'z', 'ä'].sort(localeComparer("sv")); // ["a", "z", "Z", "ä"]
+ * ```
  */
 export function localeComparer(locale: string): Comparer<string> {
   return (a, b) => sign(a.localeCompare(b, locale));
@@ -173,6 +266,11 @@ export function localeComparer(locale: string): Comparer<string> {
 
 /**
  * Order older dates before newer dates.
+ * @example
+ *
+ * ```js
+ * [new Date(2024, 2, 1), new Date(2024, 1, 1)].sort(compareDate); // [new Date(2024, 1, 1), new Date(2024, 2, 1)]
+ * ```
  */
 export const compareDate: Comparer<Date> = mapComparer(
   compareFiniteNumber,
@@ -181,6 +279,11 @@ export const compareDate: Comparer<Date> = mapComparer(
 
 /**
  * Compare booleans. True is ordered before false.
+ * @example
+ *
+ * ```js
+ * [false, true, false].sort(compareBoolean); // [true, false, false]
+ * ```
  */
 export function compareBoolean(a: boolean, b: boolean): CompareResult {
   return a === b ? 0 : a ? 1 : -1;
@@ -188,6 +291,11 @@ export function compareBoolean(a: boolean, b: boolean): CompareResult {
 
 /**
  * Invert the order of a comparer.
+ * @example
+ *
+ * ```js
+ * [1, 2, 3].sort(invertComparer(compareNumber)); // [3, 2, 1]
+ * ```
  */
 export function invertComparer<T>(comparer: Comparer<T>): Comparer<T> {
   return (a, b) => comparer(b, a);
@@ -197,6 +305,26 @@ export function invertComparer<T>(comparer: Comparer<T>): Comparer<T> {
  * Combine multiple comparers into a single comparer.
  * Comparers are iterated through until a comparer indicates the values are non-equal.
  * If all comparers return values are equal, the items are considered equal.
+ * @example
+ *
+ * ```js
+ * [{
+ *   name: "a",
+ *   country: "US"
+ * }, {
+ *   name: "c",
+ *   country: "DE"
+ * }, {
+ *   name: "b",
+ *   country: "DE"
+ * }].sort(
+ *   chainComparers(
+ *     mapComparer(compareString, (item) => item.country),
+ *     mapComparer(compareString, (item) => item.name),
+ *   )
+ * );
+ * // [{name: "b", country: "DE"}, {name: "c", country: "DE"}, {name: "a", country: "US"}]
+ * ```
  */
 export function chainComparers<T>(
   ...comparers: readonly Comparer<T>[]
@@ -214,6 +342,12 @@ export function chainComparers<T>(
 
 /**
  * Compare strings based on a predefined order.
+ * @example
+ *
+ * ```js
+ * const preferredOrder = ["high", "medium", "low"];
+ * ["low", "high", "medium", "high"].sort(orderComparer(preferredOrder)); // ["high", "high", "medium", "low"]
+ * ```
  */
 export function orderComparer<T>(ordering: readonly T[]): Comparer<T> {
   // Create a reverse lookup table for quickly finding the index of an item.
@@ -226,6 +360,14 @@ export function orderComparer<T>(ordering: readonly T[]): Comparer<T> {
 
 /**
  * Calls mapper for both values to be compared, calls comparer on the resulting values.
+ * @example
+ *
+ * ```js
+ * [{ name: "Bob" }, { name: "Charlie" }, { name: "Alice" }].sort(
+ *   mapComparer(compareString, (item) => item.name)
+ * );
+ * // [{ name: "Alice" }, { name: "Bob" }, { name: "Charlie" }]
+ * ```
  */
 export function mapComparer<TIn, TOut>(
   comparer: Comparer<TOut>,
@@ -242,6 +384,18 @@ export function mapComparer<TIn, TOut>(
  * First groups values based on keyFn.
  * Then compares values based on key from keyFn.
  * Then compares using comparison function in compareFns[key].
+ *
+ * @example
+ *
+ * ```js
+ * [1, 2, 3, 4, 5, 6].sort(
+ *   groupComparer({
+ *     even: compareNumber,
+ *     odd: invertComparer(compareNumber),
+ *   }, (value) => (value % 2 ? "odd" : "even"))
+ * );
+ * // [2, 4, 6, 5, 3, 1]
+ * ```
  */
 export function groupComparer<TKey extends Key, TValue>(
   compareFns: Record<TKey, Comparer<TValue>>,
@@ -288,6 +442,11 @@ export function arrayComparer<T>(itemComparer: Comparer<T>): Comparer<T[]> {
 /**
  * Order numbers as well as strings using compareNumber and compareString.
  * Numbers are ordered before strings.
+ * @example
+ *
+ * ```js
+ * ["b", 1, "a", 3].sort(compareNumberOrString); // [1, 3, "a", "b"]
+ * ```
  */
 export const compareNumberOrString = conditionTypeFirstComparer<
   number | string,
@@ -300,9 +459,9 @@ export const compareNumberOrString = conditionTypeFirstComparer<
  * Other parts are compared as strings.
  * @example
  * ```js
- * const strings = ["a1", "a10", "a2", "a20"];
+ * const strings = ["b1", "a1", "a10", "a2", "a20"];
  * strings.sort(compareStringNatural);
- * // => ["a1", "a2", "a10", "a20"]
+ * // => ["a1", "a2", "a10", "a20", "b1"]
  * ```
  */
 export const compareStringNatural = mapComparer(
